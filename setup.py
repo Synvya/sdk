fe9e10085121e01ec1e2084a3f112628cbfc6fed
Alejandro <alejandro@alejandrogil.net>
2024-12-30 20:58:02 -0800

    Update setup.py
    Debugging workflow

--- setup.py
+++ setup.py
@@ -2,16 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="agentstr",
-    version="0.0.1",
+    version="0.0.4",
     author="Alejandro Gil",
     description="A library for collaborative AI agents",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/synvya/agentstr",
-    packages=find_packages(),
-    install_requires=[
-        # List your dependencies here
-    ],
+    packages=find_packages(where="."),
+    install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
