a5f88d2f410ef7fb5e979dc8afaf3e23bbd5f804
Alejandro Gil <alejandro@alejandrogil.net>
2025-01-08 15:02:00 -0800

    Incorporate phidata agent library

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,16 @@
 import unittest
 import agentstr
 
-from agentstr.core import add
+from agentstr.core import add, AgentStr
+from phi.model.openai import OpenAIChat
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def test_get_public_key():
+    agent = AgentStr(company="Synvya AI", role="Seller")
+    public_key = agent.get_public_key()
+    assert public_key == "npub"
 
 def test_add():
     assert add(2, 3) == 5
