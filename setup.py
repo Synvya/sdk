247ac80414d3b5d9035d50257e374f03ea6768c8
Alejandro <alejandro@alejandrogil.net>
2025-01-08 19:33:56 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="agentstr",
-    version="0.0.8",
+    version="0.0.9",
     author="Alejandro Gil",
     description="A library for collaborative AI agents",
     long_description=open("README.md").read(),
@@ -10,7 +10,7 @@ setup(
     url="https://github.com/synvya/agentstr",
     packages=find_packages(where="."),
     install_requires=[
-        "phi>=2.7.0", "openai>=1.50.0"
+        "phidata>=2.7.0", "openai>=1.50.0"
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
