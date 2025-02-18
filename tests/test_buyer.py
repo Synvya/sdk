6b3341a583a857334f08b136a0efcfa67513d041
Alejandro Gil <alejandro@alejandrogil.net>
2025-02-17 16:59:24 -0800

    Test corrections

--- tests/test_buyer.py
+++ tests/test_buyer.py
@@ -21,15 +21,19 @@ def test_buyer_profile_creation(
 def test_find_sellers_by_location(
     buyer_tools: BuyerTools, merchant_location: str, merchant_profile_name: str
 ) -> None:
-    result = buyer_tools.find_sellers_by_location(merchant_location)
-    assert result is not None
-    assert merchant_profile_name in result
+    with patch.object(
+        buyer_tools, "_map_location_to_geohash"
+    ) as mock_map_location_to_geohash:
+        mock_map_location_to_geohash.return_value = "000000000"
+
+        result = buyer_tools.find_sellers_by_location(merchant_location)
+        assert result is not None
+        assert merchant_profile_name in result
 
 
 def test_find_seller_by_name(
     buyer_tools: BuyerTools,
     merchant_profile_name: str,
-    seller_nostr_profile: NostrProfile,
 ) -> None:
     result = buyer_tools.find_seller_by_name(merchant_profile_name)
     assert result is not None
@@ -63,8 +67,8 @@ def test_get_seller_stalls(
     ) as mock_get_seller_stalls:
         mock_get_seller_stalls.return_value = merchant_stalls
 
-    result = buyer_tools.get_seller_stalls(seller_nostr_profile.get_public_key())
-    assert result is not None
+        result = buyer_tools.get_seller_stalls(seller_nostr_profile.get_public_key())
+        assert result is not None
 
 
 def test_get_seller_products(
@@ -73,15 +77,15 @@ def test_get_seller_products(
     seller_nostr_profile: NostrProfile,
 ) -> None:
     with patch.object(
-        buyer_tools._nostr_client, "retrieve_products_from_seller"
+        buyer_tools._nostr_client,
+        "retrieve_products_from_seller",
+        return_value=merchant_products,
     ) as mock_get_seller_products:
-        mock_get_seller_products.return_value = merchant_products
+        result = buyer_tools.get_seller_products(seller_nostr_profile.get_public_key())
+        assert isinstance(result, str)  # Ensure it's a JSON string
 
-    result = buyer_tools.get_seller_products(seller_nostr_profile.get_public_key())
-    assert isinstance(result, str)  # Ensure it's a JSON string
-
-    products = json.loads(result)  # Convert JSON string back to a Python list
-    assert isinstance(products, list)  # Ensure it's a list
-    assert len(products) > 0  # Ensure the list is not empty
-    assert isinstance(products[0], dict)  # Ensure the first item is a dictionary
-    assert "name" in products[0]  # Ensure "name" key exists in the first product
+        products = json.loads(result)  # Convert JSON string back to a Python list
+        assert isinstance(products, list)  # Ensure it's a list
+        assert len(products) > 0  # Ensure the list is not empty
+        assert isinstance(products[0], dict)  # Ensure the first item is a dictionary
+        assert "name" in products[0]  # Ensure "name" key exists in the first product
